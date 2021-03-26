58bb18805906564a1cbc5063ae5d58d08709c2ed
dayz <polysemantic@gmail.com>
2021-03-26 16:53:16 +0100

    can I support resolution loops?

--- src/main/haxe/stx/di/Resolvers.hx
+++ src/main/haxe/stx/di/Resolvers.hx
@@ -1,5 +1,7 @@
 package stx.di;
 
+import stx.fn.Thunk;
+
 class Resolvers{
   @:access(stx.di) static public function resolves():String->(DI->Dynamic){
     return function(type:String):DI->Dynamic {
@@ -13,6 +15,10 @@ class Resolvers{
                 if (factory == null){
                     throw '"$type" not found: (${target.factories})';
                 }else{
+                    // var result : Dynamic = Thunk._.cache(() -> factory(target))();
+                    // if(result == null){
+                    //   throw "no result";
+                    // }
                     target.instances.set(type,factory(target));
                     inst = target.instances.get(type);
                 }
