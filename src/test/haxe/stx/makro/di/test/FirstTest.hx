767a1288ba496824110499787307ee2c86c061e5
0b1kn00b <polysemantic@gmail.com>
2023-03-30 09:00:56 +0200

    Pull out some of the old test cases

--- src/test/haxe/stx/makro/di/test/FirstTest.hx
+++ src/test/haxe/stx/makro/di/test/FirstTest.hx
@@ -12,84 +12,28 @@ class FirstTest extends TestCase{
     // });
     //trace(Dependencies.map);
   }
-  // function test(){
-  //   trace("MAIN");
-  //   var di = __.di();
-  //       Lift.add(di,
-  //         function():Int{
-  //           return 3;
-  //         }
-  //       );
-  //       di.add(
-  //         function(): { a : Bool }{
-  //           return { a : true };
-  //         }
-  //       );
-  //       di.add(
-  //         function():GenericClass<String>{
-  //           return new ConcreteGenericClass();
-  //         }
-  //       );
-        
-  //       /*di.run(
-  //         function(v:TestClass<String>){
-  //           trace(v);
-  //         }
-  //       );*/
-  //   var di2 = di.extend();
-  //       di2.add(
-  //         function():ConcreteGenericClass{
-  //           var out =  new ConcreteGenericClass();
-  //               out.value = "HI";
-  //           return out;
-  //         }
-  //       );
-  //   trace("0___________________");
-  //       /*di2.run(
+  function test(){
+    // function(v:TestClass<String>){
+    //           trace(v);
+    //         }
+
+    //       /*di2.run(
   //         function(v:ConcreteGenericClass){
   //           trace(v);
   //         }
-  //       );*/
-  //   trace("1___________________");
-  //   var di3 = di2.extend();
-  //       di3.add((a:ConcreteGenericClass,b:DependentOnChildUnitFromParent) -> new DependentOnValueInPreviousCompileUnit(a,b));
-  //       di3.add(() -> new DependentOnChildUnitFromParent());
-  //       di3.injector().run(
-  //         function(d:DependentOnValueInPreviousCompileUnit){
-  //           trace(d);
-  //         }
-  //       );
-        
-  //   trace("2___________________");    
-  //   var di4 = di3.extend();
-  //     try{
   //       di4.injector().run(
   //         function(d:WithNotFound){
 
   //         }
-  //       );
-  //     }catch(e:Dynamic){
-
-  //     }
-  //     trace("___________________");
-  //   var di5 = di4.extend();
-  //     di5.add(() -> new AbstractOverTypedef());
-  //   trace("________________________________________");
-  //   var di6 = __.di();
-  //       di6.add(
-  //         (notInAssembly:stx.makro.di.test.head.data.NotInAssembly) -> new WithNotInAssembly(notInAssembly)
-  //       );
-
-  //   var di7 = di6.extend();
-  //       di7.add(
-  //         () -> new NotInAssembly()
-  //       );
-  //       di7.injector().run(
-  //         function(v:WithNotInAssembly){
+  //di3.injector().run(
+    //         function(d:DependentOnValueInPreviousCompileUnit){
+    //           trace(d);
+    //         }
+    //       );
+    //         function(v:WithNotInAssembly){
   //           trace(v.notInAssembly.ok);
   //         }
-  //       );
-  // }
+  }
   public function testMonkey(){
     //trace(new stx.makro.di.Registry());
     #if (!macro)
@@ -106,7 +50,58 @@ class FirstTest extends TestCase{
     #end
   }
 }
-
+@:stx.makro.di.Dependencies.register(__)
+class FirstTestDeepAssembly{
+  function get_int():Int{
+    return 3;
+  }
+  function get_anonymous_structure():{ a : Bool }{
+    return { a : true };
+  }
+  function get_generic_class_implementation():GenericClass<String>{
+    return new ConcreteGenericClass();
+  }
+}
+@:stx.makro.di.Dependencies.register(__)
+class SecondTestDeepAssembly{
+  function get_second_generic_class_implementation():ConcreteGenericClass{
+    var out =  new ConcreteGenericClass();
+        out.value = "HI";
+    return out;
+  }
+}
+@:stx.makro.di.Dependencies.register(__)
+class ThirdTestDeepAssembly{
+  function get_whatever_this_is(a:ConcreteGenericClass,b:DependentOnChildUnitFromParent){
+    return new DependentOnValueInPreviousCompileUnit(a,b);
+  }
+}
+@:stx.makro.di.Dependencies.register(__)
+class FourthTestDeepAssembly{
+  function get_this_too(a:ConcreteGenericClass,b:DependentOnChildUnitFromParent){
+    return new DependentOnValueInPreviousCompileUnit(a,b);
+  }
+  function and_this(){
+    return new DependentOnChildUnitFromParent();
+  }
+}
+@:stx.makro.di.Dependencies.register(__)
+class FifthTestDeepAssembly{
+  function abstract_over_typedef(){
+    return new AbstractOverTypedef();
+  }
+}
+@:stx.makro.di.Dependencies.register(__)
+class SixthTestDeepAssembly{
+  function not_in_assembly(notInAssembly:stx.makro.di.test.head.data.NotInModule.NotInAssembly){
+    return new stx.makro.di.test.head.data.WithNotInModule.WithNotInAssembly(notInAssembly);
+  }
+}
+class SeventhTestDeepAssembly{
+  function fulfill_not_in_assembly(){
+    return new stx.makro.di.test.head.data.NotInModule.NotInAssembly();
+  }
+}
 @:stx.makro.di.Dependencies.register(__)
 class FirstTestAssembly extends stx.makro.di.core.Assembly{
 
